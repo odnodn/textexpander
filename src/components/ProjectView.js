3f844b5c5aa96402ae5e5c5002c3e4117afa8dfa
Won-Wook Hong <jradoo@gmail.com>
2017-09-09 18:22:21 +0900

    refactor ProjectView.js

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -1,45 +1,47 @@
 import React from 'react';
 
+const items = [
+  {name:"Administrative", description:"all adiministrative tasks",
+    list: [
+      {name:"home" ,list: [{name:"chores"}]},
+      {name:"buy"}
+    ]
+  },
+  {name:"Office"}
+]
+
 export default class ProjectView extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {date: new Date()};
+    this.state = {"items": items};
+  }
+
+  renderList(list) {
+    if(!list || list.length == 0)
+      return null;
+
+    const subitems = list.map((subitem, index) =>
+      this.renderItem(subitem, index)
+    )
+    return (<div className="list">
+      {subitems}
+      </div>
+    )
   }
 
   renderItem(item, index) {
-    var sublist = null;
-    if(item.list && item.list.length > 0)
-    {
-      const subitems = item.list.map((subitem, index) =>
-        this.renderItem(subitem, index)
-      )
-      sublist = (<div className="list">
-        {subitems}
-        </div>
-      )
-    }
     return (<div className="item" key={index}>
       <i className="folder icon"></i>
       <div className="content">
         <div className="header">{item.name}</div>
         <div className="description">{item.description}</div>
-        {sublist}
+        {this.renderList(item.list)}
       </div>
     </div>);
   }
 
   render() {
-    const items = [
-      {name:"Administrative", description:"all adiministrative tasks",
-        list: [
-          {name:"home"},
-          {name:"buy"}
-        ]
-      },
-      {name:"Office"}
-    ]
-
-    const list = items.map((item, index) =>
+    const list = this.state.items.map((item, index) =>
       this.renderItem(item, index)
     )
 
